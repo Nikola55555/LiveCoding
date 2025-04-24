c6375ff71c560403be51c04e46bc1a7ff6de8985
Nikola55555 <8637868@mail.ru>
2025-04-24 21:27:46 +0300

    run test

---
+++ config/links.py
@@ -0,0 +1,9 @@
+
+
+class Links:
+    HOST = "https://opensource-demo.orangehrmlive.com/web/index.php/"
+    LOGIN_PAGE = HOST + "auth/login"
+    DASHBOARD_PAGE = HOST + "dashboard/index"
+    PERSONAL_PAGE = HOST + "pim/viewPersonalDetails/empNumber/7"
+    
+    
